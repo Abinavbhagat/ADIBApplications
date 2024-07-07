5efa37083656df97653958c307766ff9f08fe564
Abinavbhagat <anuradha.na25@gmail.com>
2024-07-07 23:45:55 +0530

    initial commit

---
+++ SBSqlCurdApp/src/main/java/com/core/tech/SbSqlCurdAppApplication.java
@@ -0,0 +1,14 @@
+package com.core.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSqlCurdAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbSqlCurdAppApplication.class, args);
+		System.out.println("This is Spring Boot Curd Application");
+	}
+
+}
